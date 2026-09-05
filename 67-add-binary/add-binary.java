83375a5ca9ddf2faacf48ad9521a7d4734d5e839
Yoshita Pol <yoshitasvpol@gmail.com>
2026-09-05 23:54:34 +0530

    Time: 1 ms (99.95%) | Memory: 43.3 MB (92.55%) - LeetSync

---
+++ 67-add-binary/add-binary.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String addBinary(String a, String b){
+        int i=a.length()-1;
+        int j=b.length()-1;
+        int carry=0;
+        char[] result=new char[Math.max(a.length(), b.length())+1];
+        int k=result.length-1;
+        while(i>=0 || j>=0 || carry!=0){
+            int sum=carry;
+            if(i>=0){
+                sum+=a.charAt(i)-'0';
+                i--;
+            }
+            if(j>=0){
+                sum+=b.charAt(j)-'0';
+                j--;
+            }
+            result[k]=(char) ('0'+(sum%2));
+            k--;
+            carry=sum/2;
+        }
+        return new String(result, k + 1, result.length - k - 1);
+    }
+}
